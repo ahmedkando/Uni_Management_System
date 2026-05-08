c8c22c41bddf0db6bb0e979fbde5c19faf684bc9
Sondos Elshopaky <sondosshobaky@gmail.com>
2026-05-09 00:34:22 +0300

    updated uml

--- src/main/java/com/mycompany/advanced_project/Classes/Announcement.java
+++ src/main/java/com/mycompany/advanced_project/Classes/Announcement.java
@@ -2,24 +2,14 @@
 package com.mycompany.advanced_project.Classes;
 
 
-public class Announcement {
-    private String content;
-    private String timestamp;
+public class Announcement extends Sendable {
 
     public Announcement(String content) {
-        this.content = content;
-        this.timestamp = java.time.LocalDateTime.now().toString();
+        super(content,java.time.LocalDateTime.now().toString());
     }
 
-    public String getContent() {
-        return content;
-    }
-
-    public String getTimestamp() {
-        return timestamp;
-    }
 
     public String getInfo() {
-        return "Announcement: " + content + " (Posted: " + timestamp + ")";
+        return "Announcement: " + getContent() + " (Posted: " + getTimestamp() + ")";
     }
 }
