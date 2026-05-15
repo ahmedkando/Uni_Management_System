0c5f8046c50fbc9705058b249328a05c90ce13bf
Sondos Elshopaky <sondosshobaky@gmail.com>
2026-05-15 16:42:34 +0300

    Update TemplatePanel.java

--- src/main/java/com/mycompany/advanced_project/UI/TemplatePanel.java
+++ src/main/java/com/mycompany/advanced_project/UI/TemplatePanel.java
@@ -1,14 +1,12 @@
 package com.mycompany.advanced_project.UI;
-import javax.swing.*;
-import java.awt.*;
+import javafx.scene.layout.*;
+import javafx.scene.paint.*;
+import javafx.geometry.*;
 
-
-public class TemplatePanel extends JPanel{
-    public TemplatePanel(LayoutManager layout) {
-        setLayout(layout);
-        setOpaque(true);
-        setBackground(new Color(255, 255, 255));
-        setBorder(BorderFactory.createEmptyBorder(0, 50, 0, 0));
+public class TemplatePanel extends StackPane {
+    public TemplatePanel() {
+        setBackground(new Background(new BackgroundFill(Color.WHITE, null, null)));
+        setPadding(new Insets(0, 0, 0, 50));
     }
     
 }
