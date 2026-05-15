aeaad647cc664ad0dc91dc32aa5ffc62f61c8f46
Sondos Elshopaky <sondosshobaky@gmail.com>
2026-05-15 16:42:10 +0300

    Update PrimaryTextArea.java

--- src/main/java/com/mycompany/advanced_project/UI/PrimaryTextArea.java
+++ src/main/java/com/mycompany/advanced_project/UI/PrimaryTextArea.java
@@ -1,37 +1,35 @@
 package com.mycompany.advanced_project.UI;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.paint.*;
 
-import javax.swing.*;
-//import java.util.List;
-import java.awt.*;
-import java.awt.event.FocusAdapter;
-import java.awt.event.FocusEvent;
-import javax.swing.border.LineBorder;
+public class PrimaryTextArea extends TextArea{
+private final Border focusBorder = new Border(new BorderStroke(
+            Color.rgb(77, 163, 255),
+            BorderStrokeStyle.SOLID,
+            CornerRadii.EMPTY,
+            new BorderWidths(1)
+    ));
 
-public class PrimaryTextArea extends JTextArea{
-    public PrimaryTextArea(boolean editable){
-        if (editable==true){
-    setCursor(new Cursor(Cursor.TEXT_CURSOR));
-    setBorder(new LineBorder(Color.GRAY, 1));
-        addFocusListener(new FocusAdapter() {
-            @Override
-            public void focusGained(FocusEvent e) {
-                setBorder(new LineBorder(new Color(77, 163, 255), 1));
-            }
+    private final Border defaultBorder = new Border(new BorderStroke(
+            Color.GRAY,
+            BorderStrokeStyle.SOLID,
+            CornerRadii.EMPTY,
+            new BorderWidths(1)
+    ));
 
-            @Override
-            public void focusLost(FocusEvent e) {
-                setBorder(new LineBorder(Color.GRAY, 1));
-            }
-        });
-        }
-        else
-        {
-            setBorder(new LineBorder(Color.GRAY, 1));
+    public PrimaryTextArea(boolean editable) {
+        setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, null)));
+        setBorder(defaultBorder);
+
+        if (editable) {
+            focusedProperty().addListener((observable, oldValue, newValue) -> {
+                setBorder(newValue ? focusBorder : defaultBorder);
+            });
+        } else {
             setEditable(false);
-            setFocusable(false);
-//            setFont(new Font(Font.MONOSPACED, Font.PLAIN, 10));
-            setForeground(new Color(0, 0, 0));
-            
+            setFocusTraversable(false);
+            setMouseTransparent(true);
         }
     }
 }
